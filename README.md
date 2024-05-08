c22bd944514f89fbf81aeec5df14b449e19b8203
Sam Estrin <sam.estrin@gmail.com>
2024-05-07 18:03:02 -0700

    Major Update
    Deprecated tesseract.js and replaced it with pdf-lib
    Built out the initial README.md

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # pdf-ocr-api-digitalocean
+
+[![Star on GitHub](https://img.shields.io/github/stars/samestrin/pdf-ocr-api-digitalocean?style=social)](https://github.com/samestrin/pdf-ocr-api-digitalocean/stargazers)[![Fork on GitHub](https://img.shields.io/github/forks/samestrin/pdf-ocr-api-digitalocean?style=social) ](https://github.com/samestrin/pdf-ocr-api-digitalocean/network/members)[![Watch on GitHub](https://img.shields.io/github/watchers/samestrin/pdf-ocr-api-digitalocean?style=social)](https://github.com/samestrin/pdf-ocr-api-digitalocean/watchers)
+
+![Version 0.0.1](https://img.shields.io/badge/Version-0.0.1-blue) [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg) ](https://opensource.org/licenses/MIT)[![Built with Python](https://img.shields.io/badge/Built%20with-Python-green)](https://www.python.org/)
+
+This project implements a simulated Optical Character Recognition (OCR) service that extracts text from PDF files uploaded by users. Built with Node.js and utilizing several libraries such as Express, Multer, and pdf-parse, this application is designed to be easy to set up and integrate into other systems needing PDF text extraction capabilities.
+
+## Features
+
+- **PDF Text Extraction**: Allows users to upload PDF files and extracts readable text from them.
+- **File Upload Management**: Utilizes Multer for efficient handling of file uploads with customizable storage options.
+- **Error Handling**: Robust error management to ensure stability and provide meaningful error messages to the client.
+
+## Dependencies
+
+- **Node.js**: The script runs in a Node.js environment.
+- **express**: Web framework for Node.js.
+- **multer**: Middleware for handling multipart/form-data, used for uploading files.
+- **pdf-parse**: Library to parse and extract text from PDF files.
+- **fs.promises**: Part of Node.js File System module to handle file operations using promises.
+- **path**: Utilities for handling and transforming file paths.
+
+## Installing Node.js
+
+Before installing, ensure you have Node.js and npm (Node Package Manager) installed on your system. You can download and install Node.js from [Node.js official website](https://nodejs.org/).
+
+## Installing pdf-ocr-api-digitalocean
+
+To install and use pdf-ocr-api-digitalocean, follow these steps:
+
+Clone the Repository: Begin by cloning the repository containing the pdf-ocr-api-digitalocean to your local machine.
+
+```bash
+git clone https://github.com/samestrin/pdf-ocr-api-digitalocean/
+```
+
+Set PORT environment variable to define the port on which the server will listen. Default is 3000
+
+Navigate to your project's root directory and run:
+
+```bash
+npm start
+```
+
+## Contribute
+
+Contributions to this project are welcome. Please fork the repository and submit a pull request with your changes or improvements.
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+## Share
+
+[![Twitter](https://img.shields.io/badge/X-Tweet-blue)](https://twitter.com/intent/tweet?text=Check%20out%20this%20awesome%20project!&url=https://github.com/samestrin/pdf-ocr-api-digitalocean) [![Facebook](https://img.shields.io/badge/Facebook-Share-blue)](https://www.facebook.com/sharer/sharer.php?u=https://github.com/samestrin/pdf-ocr-api-digitalocean) [![LinkedIn](https://img.shields.io/badge/LinkedIn-Share-blue)](https://www.linkedin.com/sharing/share-offsite/?url=https://github.com/samestrin/pdf-ocr-api-digitalocean)
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const multer = require("multer");
-const { getAvailableLanguages, createWorker } = require("tesseract.js");
+const pdf = require("pdf-parse");
 const app = express();
 const port = process.env.PORT || 3000;
+const fs = require("fs").promises;
+const path = require("path");
+
+const uploadsDir = path.join(__dirname, "uploads");
+
+// setup uploadsDir
+ensureDirectoryExists(uploadsDir).catch(console.error);
 
 // Set up storage for file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, uploadsDir);
   },
   filename: (req, file, cb) => {
     cb(null, file.fieldname + "-" + Date.now() + file.originalname);
@@ -16,52 +23,61 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-// OCR worker
-const worker = createWorker({
-  logger: (m) => console.log(m), // Add logger here to view OCR progress
-});
-
-app.get("/languages", async (req, res) => {
-  try {
-    await worker.load();
-    const languages = await getAvailableLanguages(worker);
-    await worker.terminate();
-
-    res.status(200).json({
-      message: "Supported Languages",
-      languages: languages,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Failed to retrieve supported languages");
-  }
-});
-
+/**
+ * Handles the OCR endpoint to process PDF and extract text.
+ * The function receives a file uploaded by the user. If a file is not uploaded, it sends a 400 status code response.
+ * If a file is uploaded, it attempts to read the file, extract text using `pdf-parse`, and send it back to the client.
+ * It also handles cleaning up the file after processing by deleting it. Errors during the process result in a 500 status code response.
+ *
+ * @param {Request} req - The express request object containing file information.
+ * @param {Response} res - The express response object used to send responses back to the client.
+ * @throws {Error} Sends a 500 status response if reading, parsing, or deleting the file fails.
+ */
 app.post("/ocr", upload.single("file"), async (req, res) => {
-  const lang = req.body.language || "eng";
-  if (req.file) {
-    try {
-      await worker.load();
-      await worker.loadLanguage(lang);
-      await worker.initialize(lang);
-      const {
-        data: { text },
-      } = await worker.recognize(req.file.path);
-      await worker.terminate();
+  if (!req.file) {
+    return res.status(400).send("No file uploaded.");
+  }
 
-      res.status(200).json({
-        message: "OCR Result",
-        OCRText: text,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Error processing the file");
-    }
-  } else {
-    res.status(400).send("No file uploaded.");
+  try {
+    const pdfPath = req.file.path;
+    const dataBuffer = await fs.readFile(pdfPath);
+
+    const results = await pdf(dataBuffer);
+    res.send({ text: results.text });
+    await fs.unlink(pdfPath); // Clean up file after processing using fs.promises
+  } catch (error) {
+    res.status(500).send("Failed to extract text from PDF.");
   }
 });
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
 });
+
+/**
+ * Sets up the directory for uploads by ensuring it exists or creating it if it does not.
+ * This function attempts to access the directory specified by `dir`. If it does not exist,
+ * the function creates it with recursive option enabled. It logs the creation of the directory.
+ *
+ * @param {string} dir - The path of the directory to check or create.
+ * @throws {Error} Throws if there is an error accessing or creating the directory that is not related to the directory's existence.
+ *
+ * @example
+ * // Ensure that the uploads directory exists
+ * ensureDirectoryExists(path.join(__dirname, "uploads")).catch(console.error);
+ */
+async function ensureDirectoryExists(dir) {
+  try {
+    // Try accessing the directory to check if it exists
+    await fs.access(dir);
+  } catch (error) {
+    // If the error code is 'ENOENT', the directory does not exist and needs to be created
+    if (error.code === "ENOENT") {
+      await fs.mkdir(dir, { recursive: true });
+      console.log(`Directory created: ${dir}`);
+    } else {
+      // Re-throw the error if it's not related to the existence of the directory
+      throw error;
+    }
+  }
+}
